9a14e5bfaf88b3819d1698c389a04a1d9332cbbc
Sam Plews <the@codeite.com>
2017-05-18 10:37:56 +0100

    make gift box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import $ from 'jquery';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      isOpen: false
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,11 +19,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>Click the box to open it</div>
+        <GiftBox isOpen={this.state.isOpen} openBox={() => this.setState({isOpen: true})}/>
       </div>
     );
+  }
+}
+
+class GiftBox extends Component {
+  componentDidUpdate() {
+    if (this.props.isOpen) {
+      $('.GiftBox-img').fadeOut(3000)
+    }
+  }
+
+  render () {
+    return <div className='GiftBox'>
+      <img className='GiftBox-img' src='/gift_box_red.jpg' alt='box' onClick={() => this.props.openBox()}/>
+    </div>
   }
 }
 
